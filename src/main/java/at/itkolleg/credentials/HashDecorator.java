03a4180df8b1556f914167da9b90e8f4105202b7
Christoph Schneider <herbertchristoph@gmail.com>
2022-11-16 08:30:36 +0100

    Abstrakte Klasse HashDecorator ausimplementiert und kommentiert

---
+++ src/main/java/at/itkolleg/credentials/HashDecorator.java
@@ -0,0 +1,16 @@
+package at.itkolleg.credentials;
+/**
+ * Diese abstrakte Klasse HashDecorator erweitert das Interface ExportCredentials
+ */
+public abstract class HashDecorator implements ExportCredentials{
+    // Hält das Interface ExportCredentials
+    private ExportCredentials exportCredentials;
+
+    /**
+     * Konstruktur setzt das Datenfeld ExportCredentials mit dem übergebenen Interface ExportCredentials
+     * @param exportCredentials ein Interface von ExportCredentials
+     */
+    public HashDecorator(ExportCredentials exportCredentials) {
+        this.exportCredentials = exportCredentials;
+    }
+}
